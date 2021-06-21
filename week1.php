e38b63db4599e0448dd13c4da5362ed9501d1c35
shell_bot_cheng <639174779@qq.com>
2021-06-21 17:46:25 +0800

    Create week1.php

---
+++ week1.php
@@ -0,0 +1,19 @@
+class Solution {
+
+    /**
+	 * 66 加-
+     * @param Integer[] $digits
+     * @return Integer[]
+     */
+    function plusOne($digits) {
+        for($i = count($digits)-1; $i >= 0 ;$i--){
+            $digits[$i]++;
+            $digits[$i] = $digits[$i] % 10;
+            if($digits[$i] != 0){
+                return $digits;
+            }
+        }
+        array_unshift($digits, 1);
+        return $digits;
+    }
+}
