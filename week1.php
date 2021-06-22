77c2a30025ba47508eb6ff267f7d37911a970c0a
程振远 <639174779@qq.com>
2021-06-22 23:34:06 +0800

    作业提交

--- week1.php
+++ week1.php
@@ -17,6 +17,34 @@ class Solution {
         array_unshift($digits, 1);
         return $digits;
     }
-	
+
+
+	/**
+     * 560. 和为K的子数组
+     * @param Integer[] $nums
+     * @param Integer $k
+     * @return Integer
+     */
+    function subarraySum($nums, $k) {
+        $n = count($nums);
+        if ($n == 0) {
+            return 0;
+        }
+        $ans = 0;
+        $arr = [];
+        $arr[0] = 1;
+        $preSum = 0;
+        for ($i = 0; $i < $n; $i++) {
+            $preSum += $nums[$i];
+            $diff = $preSum - $k;
+            isset($arr[$diff]) && $ans+= $arr[$diff];
+            if (isset($arr[$preSum])) {
+                $arr[$preSum]++;
+            } else {
+                $arr[$preSum] = 1;
+            }
+        }
+        return $ans;
+    }
 
 }
